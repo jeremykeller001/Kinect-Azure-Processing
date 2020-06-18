b6e81f4fe69761d4a1c9aa0b7ad1bd38d79fb365
Jeremy Keller <jeremykeller001@gmail.com>
2020-06-18 00:26:43 -0600

    doc BodyTrackingUtils.h

--- src/BodyTrackingUtils.h
+++ src/BodyTrackingUtils.h
@@ -29,11 +29,16 @@
 #ifndef BODY_TRACKING_UTILS_H
 #define BODY_TRACKING_UTILS_H
 
+/**
+* Utilities to obtain joint locations from body tracking data and perform calculations based on them
+*/
 class BodyTrackingUtils
 {
 public:
-	// Eddy Rogers
-	//Struct to contain the minimum and maximum acceptable x, y, and z values
+	/**
+	* Struct to contain the minimum and maximum acceptable x, y, and z values for a point cloud.
+	* It is intended to be used to filter out points outside of the relevant capture space.
+	*/
 	struct BoundingBox {
 		double xMin;
 		double xMax;
@@ -43,10 +48,33 @@ public:
 		double zMax;
 	};
 
+	/**
+	* Predict the joints of a frame and output the locations to a json object
+	*
+	* @param jsonFrames Json object to append the joint locations of the frame to
+	* @param frameCount The frame group number of the joint locations being predicted
+	* @param tracker Body tracker
+	* @param capture_handle Capture to process
+	* @param jointPositions Container that will hold the predicted joint locations to be used for later processing
+	* @return Whether or not joints could be predicted for the capture frame
+	*/
 	static bool predictJoints(boost::property_tree::ptree* jsonFrames, int frameCount, k4abt_tracker_t tracker, k4a_capture_t capture_handle, std::vector<Eigen::RowVector3d>* jointPositions);
 
+	/**
+	* Generates a BoundingBox based on joint locations so that a subject may be isolated within a capture
+	*
+	* @param jointPositions Joint positions to use to generate the BoundingBox with
+	* @return BoundingBox object with xyz min/max values populated based on the joint positions
+	*/
 	static BoundingBox createBoundingBox(std::vector<Eigen::RowVector3d> jointPositions);
 
+	/**
+	* Checks if a point is within the bounds of a BoundingBox
+	*
+	* @param point XYZ coordinate point to check
+	* @param bounds BoundingBox to check the point against
+	* @return Whether or not the point given is within the bounds of the BoundingBox
+	*/
 	static bool withinBounds(Eigen::RowVector3d point, BoundingBox bounds);
 	
 };
